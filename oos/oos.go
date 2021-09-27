cd6fc6a4558a1c30df475cff10b19c22afdae24c
Crow <5488190+echocrow@users.noreply.github.com>
2021-09-27 23:45:28 +0200

    Extend pkg doc

--- oos/oos.go
+++ oos/oos.go
@@ -1,4 +1,8 @@
 // Package oos provides the original OS abstraction implementation.
+//
+// This package simply wraps and calls the default "os" functions of the
+// standard library. This is the default "osa" implementation, so typically code
+// does not need to import or directly interact with this package.
 package oos
 
 import "os"
